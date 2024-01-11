1a312243c619a8a17ee7e4d835560159a290c049
Eliecer Arteaga <eliecerj@gmail.com>
2024-01-11 20:55:18 -0300

    adding friend functionality

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -29,17 +29,23 @@ function Button({ children, onClick }) {
   );
 }
 export default function App() {
+  const [friends, setFriends] = useState(initialFriends);
   const [showAddFriend, setShowAddFriend] = useState(false);
 
   function handleShowAddFriend() {
     setShowAddFriend((show) => !showAddFriend);
   }
 
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriend(false);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        {showAddFriend && <FormAddFriend />}
+        <FriendsList friends={friends} />
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "Close" : "Add Friend"}
         </Button>
@@ -49,8 +55,7 @@ export default function App() {
   );
 }
 
-function FriendsList() {
-  const friends = initialFriends;
+function FriendsList({ friends }) {
   return (
     <ul>
       {friends.map((friend) => (
@@ -82,7 +87,7 @@ function Friend({ friend }) {
   );
 }
 
-function FormAddFriend() {
+function FormAddFriend({ onAddFriend }) {
   const [name, setName] = useState("");
   const [image, setImage] = useState("https://i.pravatar.cc/48");
 
@@ -97,6 +102,7 @@ function FormAddFriend() {
       image: `${image}?=${id}`,
       balance: 0,
     };
+    onAddFriend(newFriend);
     console.log(newFriend);
     setName("");
     setImage("https://i.pravatar.cc/48");
